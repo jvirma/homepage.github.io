6b704eff9220ee8e0bc231bbcfc743d7a152f509
jvirma <jorbeful@gmail.com>
2020-05-29 00:01:35 +0300

    Updates

---
+++ precache-manifest.9ca51931ab6d583b8f40b0846fd12f84.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4ae8af8f3c82263805942d90c9ce573",
+    "url": "/index.html"
+  },
+  {
+    "revision": "491c074fa25b2d21cd74",
+    "url": "/static/css/main.b171f7b7.chunk.css"
+  },
+  {
+    "revision": "f1853bf5125756a54f02",
+    "url": "/static/js/2.dc90d855.chunk.js"
+  },
+  {
+    "revision": "cf89bcfdc86db2c261a743301fbeb514",
+    "url": "/static/js/2.dc90d855.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "491c074fa25b2d21cd74",
+    "url": "/static/js/main.7162d623.chunk.js"
+  },
+  {
+    "revision": "edcffb4e586786910335",
+    "url": "/static/js/runtime-main.b2fe0743.js"
+  },
+  {
+    "revision": "5c7ceff3ca0ed04aa124cfea23a5f282",
+    "url": "/static/media/avatar.5c7ceff3.jpg"
+  },
+  {
+    "revision": "d68281a55c06f2a4f4d5397dd02f6ef2",
+    "url": "/static/media/home.d68281a5.png"
+  }
+]);
