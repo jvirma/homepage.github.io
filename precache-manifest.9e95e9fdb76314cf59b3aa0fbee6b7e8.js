4f2a3cc4639f2dc282e3d8b0d82981785e58f615
jvirma <jorbeful@gmail.com>
2020-05-29 14:17:11 +0300

    Updates

---
+++ precache-manifest.9e95e9fdb76314cf59b3aa0fbee6b7e8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6bffc6322066e3a1d80ca2fd81dbdb7",
+    "url": "/homepage/index.html"
+  },
+  {
+    "revision": "f938e5d3b4f0acf76ccc",
+    "url": "/homepage/static/css/main.be27cd7c.chunk.css"
+  },
+  {
+    "revision": "fcd2033748d92b413ba4",
+    "url": "/homepage/static/js/2.397966a6.chunk.js"
+  },
+  {
+    "revision": "cf89bcfdc86db2c261a743301fbeb514",
+    "url": "/homepage/static/js/2.397966a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f938e5d3b4f0acf76ccc",
+    "url": "/homepage/static/js/main.3f421107.chunk.js"
+  },
+  {
+    "revision": "e410ac814002d1aefe93",
+    "url": "/homepage/static/js/runtime-main.ae933cb9.js"
+  },
+  {
+    "revision": "5c7ceff3ca0ed04aa124cfea23a5f282",
+    "url": "/homepage/static/media/avatar.5c7ceff3.jpg"
+  },
+  {
+    "revision": "d68281a55c06f2a4f4d5397dd02f6ef2",
+    "url": "/homepage/static/media/home.d68281a5.png"
+  },
+  {
+    "revision": "070c8e073714ba8d0ad85cf6408dc858",
+    "url": "/homepage/static/media/logo5.070c8e07.png"
+  }
+]);
